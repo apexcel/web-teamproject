0bcfb7f38828ed390a343be79fe49c06501cffbe
oogie826 <oogie826@gmail.com>
2020-02-16 19:05:39 +0900

    Initial Commit

--- src/client/components/Footer.jsx
+++ src/client/components/Footer.jsx
@@ -12,18 +12,22 @@ const Footer = () => {
 
                 <div className='footer-item left'>
                     <div className='left-item'>
-                        <span className='f-link'>WORKS</span>
-                        <span className='f-link'>ABOUT</span>
-                        <span className='f-link'>CONTACT</span>
+                        <a href='#' className='f-link'>WORKS</a>
+                        <a href='#' className='f-link'>ABOUT</a>
+                        <a href='#' className='f-link'>CONTACT</a>
                     </div>
                 </div>
-                <div className='footer-item right'>
-                    <div className='right-item'>
+                <div className='footer-item mid'>
+                    <div className='mid-item'>
                         <span>LOCATION</span>
-                        <span>137, Hanshindae-gil, Osan-si</span>
+                        <span>18206-1, 137, Hanshindae-gil, Osan-si</span>
                         <span>Gyeonggi-do, Republic of Korea</span>
                     </div>
                 </div>
+                <a href='#' className='footer-item right'>
+                    <div className='right-item'>
+                    </div>
+                </a>
             </div>
         </footer>
     )
