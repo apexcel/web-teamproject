7aee50cbfd149ced75ae937a1bdd36ea19eedb26
oogie826 <oogie826@gmail.com>
2020-03-16 17:33:00 +0900

    Update image assets

--- src/client/images/images.js
+++ src/client/images/images.js
@@ -2,12 +2,16 @@ import WdImg1 from '../images/wdimg1.jpg'
 import WdImg2 from '../images/wdimg2.jpg'
 import WdImg3 from '../images/wdimg3.jpg'
 
-import MtImg1 from '../images/mt/mtimg1.jpg'
-import MtImg2 from '../images/mt/mtimg2.jpg'
-import MtImg3 from '../images/mt/mtimg3.jpg'
+import LsImg1 from '../images/lsimg1.jpg'
+import LsImg2 from '../images/lsimg2.jpg'
+import LsImg3 from '../images/lsimg3.jpg'
+
+import MtImg1 from '../images/mtimg1.jpg'
+import MtImg2 from '../images/mtimg2.jpg'
+import MtImg3 from '../images/mtimg3.jpg'
 
 export default [
     WdImg1, WdImg2, WdImg3,
-    MtImg1, MtImg2, MtImg3,
-    
+    LsImg1, LsImg2, LsImg3,
+    MtImg1, MtImg2, MtImg3,    
 ];
